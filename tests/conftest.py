a6f85d58d72d454ffe30466e36c92152a0e7e0e4
rattus-aristarchus <lankin.m.a@gmail.com>
2023-11-11 18:36:18 +0300

    a browser on selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,58 @@
+import os
+
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import dotenv
 
 
-@pytest.fixture(autouse=True)
-def setup_browser():
-    browser.config.driver = webdriver.Chrome()
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    dotenv.load_dotenv()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        help="Браузер для запуска тестов",
+        choices=["firefox", "chrome"],
+        default="chrome"
+    )
+    parser.addoption(
+        "--browser_version",
+        help="Версия браузера",
+        default="100.0"
+    )
+
+
+@pytest.fixture(scope="session")
+def setup_browser(request):
     browser.config.window_width = 1600
     browser.config.window_height = 1200
+    browser_name = request.config.getoption("--browser")
+    browser_version = request.config.getoption("--browser_version")
 
-    yield
+    login = os.getenv('LOGIN')
+    pwd = os.getenv('PASSWORD')
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": browser_name,
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{pwd}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+
+    yield browser
 
     browser.quit()
